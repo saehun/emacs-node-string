7a107c09ccd83a19d0c85b2dedd6b2727435a959
minidonut <nycom13@gmail.com>
2021-02-28 02:23:01 +0900

    add elisp generator

---
+++ scripts/build-elisp.js
@@ -0,0 +1,26 @@
+const Case = require('case');
+const fs = require('fs');
+const R = require('ramda');
+
+const commands = R.pipe(
+  R.map(filename => filename.replace(/\..*$/, '')),
+  R.filter(filename => filename !== 'interface'),
+  R.uniq
+)(fs.readdirSync('src/commands'));
+
+const elispProgram = body => `;;; package --- node-string
+;;; Commentary:
+;;; Code:
+${body}
+
+(provide 'node-string)
+;;; node-string.el ends here
+`;
+
+const elispCommand = command => `
+(defun node-string/${command} (&optional b e)
+  "${Case.title(command)}."
+  (interactive "r")
+  (call-process-region b e "node-string-commands" t t nil "${command}"))`;
+
+fs.writeFileSync('node-string.el', elispProgram(commands.map(elispCommand).join('')));
